8146c2c695a36d75bf33224bb44075d78c038f2c
murad-aust <murad.rony15@gmail.com>
2021-01-06 22:52:15 +0600

    first commit

---
+++ routes/shops.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const shopController = require('../controllers/shop.controller');
+const userController = require('../controllers/user.controller');
+
+/* GET users listing. */
+router.post('/', userController.isAuthenticated, shopController.create);
+router.get('/', shopController.getAll);
+router.get('/:id', shopController.getById);
+router.put('/:id', shopController.updateById);
+router.delete('/:id', shopController.deleteById);
+
+module.exports = router;
